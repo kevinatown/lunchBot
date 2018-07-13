83b6f3c6ece793793e845779a3f77a9dfc727708
Glitch (rainbow-quotation) <none>
2018-07-13 17:54:08 +0000

    🌾🏚 Updated with Glitch

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -35,6 +35,34 @@ module.exports = function(controller) {
         stats.convos++;
     });
 
+    controller.hears([new RegExp(/((?<!not )pizza)/i)], 'ambient,mention', function(bot, message) {
+      const pizzas = [
+        'https://media.giphy.com/media/aCKMaeduKfFXG/giphy.gif',
+        'https://media.giphy.com/media/1108D2tVaUN3eo/giphy.gif',
+        'https://media.giphy.com/media/e2AKpOvx2MREY/giphy.gif',
+        'https://media.giphy.com/media/3o7btXJrqLo5bbtQDm/giphy.gif',
+        'https://media.giphy.com/media/OZpBpm7c2AGFG/giphy.gif',
+        'https://media.giphy.com/media/OJ8hVSLYbpQ08/giphy.gif',
+        'https://media.giphy.com/media/eMeMQ0Y4DHS2k/giphy.gif',
+        'https://media.giphy.com/media/iJa6kOfJ3qN7a/giphy.gif',
+        'https://media.giphy.com/media/114ugL1hv5p8He/giphy.gif',
+        'https://media.giphy.com/media/wqHXbOcynM7Oo/giphy.gif',
+        'https://media.giphy.com/media/BPofSmBGnwJIQ/giphy.gif',
+        'https://media.giphy.com/media/mZWAgG8U4sXzW/giphy.gif',
+        'https://giphy.com/gifs/kVXtSmeOZoRIQ/html5',
+        'https://media.giphy.com/media/xsBP0RdvxJfhu/giphy.gif',
+        'https://media.giphy.com/media/9B5EkgWrF4Rri/giphy.gif',
+        'https://media.giphy.com/media/QIghDKst8OMko/giphy.gif',
+        'https://media.giphy.com/media/UBOnpTafKwECI/giphy.gif',
+        'https://media.giphy.com/media/3o85g0xzxEhCnfYgiQ/giphy.gif',
+        'https://media.giphy.com/media/xTiTnCxAxq0SxKWVfa/giphy.gif',
+        'https://media.giphy.com/media/3o7TKJG8p39T1hJFLO/giphy.gif',
+        'https://media.giphy.com/media/werVqqNW4mixG/giphy.gif',
+        'https://media.giphy.com/media/AeWntMyxGFXXi/source.gif'
+      ];
+      console.log(_.sample(pizzas));
+      bot.reply(message, `${_.sample(pizzas)}`);   
+    });
 
     controller.hears(['^uptime','^debug'], 'direct_message,direct_mention', function(bot, message) {
 
@@ -72,6 +100,23 @@ module.exports = function(controller) {
       }
     });
   
+    // controller.hears([new RegExp(/^shame (<@[A-Z0-9]*>)(\s\d)?/i)], 'direct_message,direct_mention,ambient,mention', function(bot, message) {
+    //     if (message.match[1] && message.match[1] !== '<@UAUUZAUCD>') {
+    //       const times = 0;
+    //       const name = message.match[1];
+    //       const maxShames = message.match[2] ? parseInt(_.trim(message.match[2]), 10) : 3;
+    //       bot.reply(message, `*SHAME ${name}*`);
+    //       setTimeout(() => shameSomeone(maxShames, name, times, message, bot), 1000);
+    //     } else if (message.match[1] && message.match[1] === '<@UAUUZAUCD>') {
+    //       const times = 0;
+    //       const name = `<@${message.event.user}>`;
+    //       const maxShames = message.match[2] ? parseInt(_.trim(message.match[2]), 10) : 3;
+    //       bot.reply(message, `*HAHA NICE TRY! ${name}*`);
+    //       bot.reply(message, `*SHAME ${name}*`);
+    //       setTimeout(() => shameSomeone(maxShames, name, times, message, bot), 1000);
+    //     }
+    // });
+  
     
     controller.hears([new RegExp(/^shame (<@[A-Z0-9]*>)(\s\d)?/i)], 'direct_message,direct_mention,ambient,mention', function(bot, message) {
         if (message.match[1] && message.match[1] !== '<@UAUUZAUCD>') {
@@ -89,6 +134,12 @@ module.exports = function(controller) {
           setTimeout(() => shameSomeone(maxShames, name, times, message, bot), 1000);
         }
     });
+  
+  controller.hears([new RegExp(/(lunchbot)/i)], 'ambient,mention', function(bot, message) {
+    bot.reply(message, `*SPEAK MORTAL FOR I AM THE TRUE LUNCHBOT*`);    
+  });
+  
+  
 
 
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
